0a9b685436176591d6929a016bfa443a8e9fda6e
Benjamin Yeh <bentyeh@users.noreply.github.com>
2023-04-26 17:30:05 -0700

    md2HTML.sh: only regenerate existing HTML file if Markdown file is newer
    Add support for arguments -c and -f for cleaning existing HTML or forcing regeneration of all HTML files

--- md2HTML.sh
+++ md2HTML.sh
@@ -1,25 +1,61 @@
 #!/bin/bash
-set -e -o pipefail
+# Convert all Markdown note files to HTML using pandoc.
+# - By default, only regenerates an existing HTML file if the Markdown file is newer.
+# - Assumes pandoc is in PATH
+# 
+# Supported arguments (mutually exclusive)
+# -c, --clean: remove existing HTML files and exit
+# -f, --force: (re)generate all HTML files
 
-# add pandoc to PATH
-source "${HOME}/.bashrc"
+# SETUP
+set -e -o pipefail
 
 # get directory of this script, which is located with the markdown files
 directory=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# (If specified) CLEAN
+toclean="$1"
+if [ ! -z "$toclean" ] && ([ "$toclean" = "-c" ] || [ "$toclean" = "--clean" ]); then
+    # need to check for existence of HTML files first, otherwise rm will complain
+    # solution from https://unix.stackexchange.com/a/214066
+    html_files=("${directory}"/*.html)
+    [ -f ${html_files[0]} ] && rm "${directory}"/*.html
+    html_files=("${directory}"/*/*.html)
+    [ -f ${html_files[0]} ] && rm "${directory}"/*/*.html
+    exit 0
+fi
+
+# CONVERT MARKDOWN TO HTML
+force="$1"
+if [ ! -z "$force" ]; then
+    if [ ! "$force" = "-f" ] && [ ! "$force" = "--force" ]; then
+        echo "Did not understand argument $force"
+        exit 1
+    fi
+    force=true
+else
+    force=false
+fi
+
 for path in "${directory}"/*.md; do
     file="$(basename "$path")"
     filename="${file%.*}"
+    output_path="${path%.*}.html"
     if [[ "$file" == "README.md" ]]; then
-        sed -e 's/\.md/.html/g' "$path" | \
-        pandoc -s --mathjax \
-            --toc --variable toc-title="Contents" --metadata title="$filename" \
-            -f gfm -t html -o "${filename}.html"
+        if [ "$force" = true ] || [ ! -f "$output_path" ] || [ "$path" -nt "$output_path" ]; then
+            echo "$filename"
+            sed -e 's/\.md/.html/g' "$path" | \
+            pandoc -s --mathjax \
+                --toc --variable toc-title="Contents" --metadata title="$filename" \
+                -f gfm -t html -o "$output_path"
+        fi
     elif [[ ! $file =~ "LICENSE.md" ]]; then
-        echo "$filename"
-        pandoc -s --mathjax \
-            --toc --variable toc-title="Contents" --metadata title="$filename" \
-            -f gfm -t html -o "${filename}.html" "$path"
+        if [ "$force" = true ] || [ ! -f "$output_path" ] || [ "$path" -nt "$output_path" ]; then
+            echo "$filename"
+            pandoc -s --mathjax \
+                --toc --variable toc-title="Contents" --metadata title="$filename" \
+                -f gfm -t html -o "$output_path" "$path"
+        fi
     fi
 done
 
@@ -28,8 +64,10 @@ for path in "${directory}"/*/*.md; do
     subdir="$(basename "$(dirname "$path")")"
     filename="${file%.*}"
     output_path="${path%.*}.html"
-    echo "${subdir}/${filename}"
-    pandoc -s --mathjax \
-        --toc --variable toc-title="Contents" --metadata title="$filename" \
-        -f gfm -t html -o "${output_path}" "$path"
+    if [ "$force" = true ] || [ ! -f "$output_path" ] || [ "$path" -nt "$output_path" ]; then
+        echo "${subdir}/${filename}"
+        pandoc -s --mathjax \
+            --toc --variable toc-title="Contents" --metadata title="$filename" \
+            -f gfm -t html -o "$output_path" "$path"
+    fi
 done
